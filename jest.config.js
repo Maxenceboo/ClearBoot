a4f7f01c648f05adc844f5124bc23ee0149bbc82
Maxence <maxenceboo@gmail.com>
2026-01-14 16:39:34 +0100

    feat: add coverage threshold (80%)

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,13 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/test/**/*.test.ts'],
-    collectCoverageFrom: ['src/**/*.ts', '!src/**/index.ts', '!src/app/main.ts']
+    collectCoverageFrom: ['src/**/*.ts', '!src/**/index.ts', '!src/app/main.ts'],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80
+        }
+    }
 };
